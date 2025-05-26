56da5ab74f19e5eead078b7bd5bafbf4f06788ec
Adrián <nagyivan.adrian@gmail.com>
2025-05-27 00:02:12 +0200

    Trying to make a Dockerfile to try to make backend online so it can work with frontend

--- src/main/java/MaintanceErrorReportingSystem/config/WebConfig.java
+++ src/main/java/MaintanceErrorReportingSystem/config/WebConfig.java
@@ -1,23 +1,32 @@
 package MaintanceErrorReportingSystem.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
+@ComponentScan("MaintanceErrorReportingSystem")
 public class WebConfig implements WebMvcConfigurer {
+
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
-            }
-        };
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
 
